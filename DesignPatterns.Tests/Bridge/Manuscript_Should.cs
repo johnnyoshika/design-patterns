7aef5ec47ff2a205e25963a4177de259a0b6b9d6
Johnny Oshika <johnnyoshika@gmail.com>
2019-10-16 08:02:37 -0700

    Add Bridge example

---
+++ DesignPatterns.Tests/Bridge/Manuscript_Should.cs
@@ -0,0 +1,72 @@
+﻿using DesignPatterns.Bridge;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace DesignPatterns.Tests.Bridge
+{
+    public class Manuscript_Should
+    {
+        // IFormatter bridge allows us to replace the formatter to change the print formatting
+        // without changing any other code.
+
+        // Common usages:
+        // - Drawing API for shapes
+        // - Persistent types for objects
+        // - .Net Provider Model
+
+        [Fact]
+        public void Use_IFormatter_As_Bridge_For_Standard_Formatting()
+        {
+            var standardFormatter = new Mock<IFormatter>();
+            standardFormatter.Setup(f => f.Format(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns<string, string>((key, value) => $"{key}: {value}");
+
+            var manuscripts = new List<Manuscript>
+            {
+                new Book(standardFormatter.Object, "Hamlet", "Shakespeare"),
+                new TermPaper(standardFormatter.Object, "History", "Bob", "Lorem ipsum dolor sit amet")
+            };
+
+            Assert.Equal("Title: Hamlet | Author: Shakespeare", manuscripts.ElementAt(0).Print());
+            Assert.Equal("Class: History | Student: Bob | Text: Lorem ipsum dolor sit amet", manuscripts.ElementAt(1).Print());
+        }
+
+        [Fact]
+        public void Use_IFormatter_As_Bridge_For_Backwards_Formatting()
+        {
+            var backwardsFormatter = new Mock<IFormatter>();
+            backwardsFormatter.Setup(f => f.Format(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns<string, string>((key, value) => $"{key}: {new string(value.Reverse().ToArray())}");
+
+            var manuscripts = new List<Manuscript>
+            {
+                new Book(backwardsFormatter.Object, "Hamlet", "Shakespeare"),
+                new TermPaper(backwardsFormatter.Object, "History", "Bob", "Lorem ipsum dolor sit amet")
+            };
+
+            Assert.Equal("Title: telmaH | Author: eraepsekahS", manuscripts.ElementAt(0).Print());
+            Assert.Equal("Class: yrotsiH | Student: boB | Text: tema tis rolod muspi meroL", manuscripts.ElementAt(1).Print());
+        }
+
+        [Fact]
+        public void Use_IFormatter_As_Bridge_For_Fancy_Formatting()
+        {
+            var fancyFormatter = new Mock<IFormatter>();
+            fancyFormatter.Setup(f => f.Format(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns<string, string>((key, value) => $"#{key}# - {value}");
+
+            var manuscripts = new List<Manuscript>
+            {
+                new Book(fancyFormatter.Object, "Hamlet", "Shakespeare"),
+                new TermPaper(fancyFormatter.Object, "History", "Bob", "Lorem ipsum dolor sit amet")
+            };
+
+            Assert.Equal("#Title# - Hamlet | #Author# - Shakespeare", manuscripts.ElementAt(0).Print());
+            Assert.Equal("#Class# - History | #Student# - Bob | #Text# - Lorem ipsum dolor sit amet", manuscripts.ElementAt(1).Print());
+        }
+    }
+}
---
+++ DesignPatterns/Bridge/Manuscript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Bridge
+{
+    public abstract class Manuscript
+    {
+        public Manuscript(IFormatter formatter) => Formatter = formatter;
+
+        protected IFormatter Formatter { get; }
+
+        public abstract string Print();
+    }
+
+    public class Book : Manuscript
+    {
+        public Book(IFormatter formatter, string title, string author) : base(formatter) =>
+            (Title, Author) = (title, author);
+
+        public string Title { get; }
+        public string Author { get; }
+
+        public override string Print() => $"{Formatter.Format("Title", Title)} | {Formatter.Format("Author", Author)}";
+    }
+
+    public class TermPaper : Manuscript
+    {
+        public TermPaper(IFormatter formatter, string @class, string student, string text) : base(formatter) =>
+            (Class, Student, Text) = (@class, student, text);
+
+        public string Class { get; }
+        public string Student { get; }
+        public string Text { get; }
+
+        public override string Print() => $"{Formatter.Format("Class", Class)} | {Formatter.Format("Student", Student)} | {Formatter.Format("Text", Text)}";
+    }
+}
