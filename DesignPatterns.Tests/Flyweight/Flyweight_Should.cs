6db2c93b1b09efa25b92cda627799cee0ca5dcae
Johnny Oshika <johnnyoshika@gmail.com>
2019-10-29 09:27:51 -0700

    Add Flyweight example
    The pluralsight video was hard to understand, so used this example instead: https://refactoring.guru/design-patterns/flyweight/csharp/example

---
+++ DesignPatterns.Tests/Flyweight/Flyweight_Should.cs
@@ -0,0 +1,63 @@
+﻿using DesignPatterns.Flyweight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPatterns.Tests.Flyweight
+{
+    public class Flyweight_Should
+    {
+        [Fact]
+        public void Use_Intrinsic_And_Extrinsic_States()
+        {
+            // The client code usually creates a bunch of pre-populated
+            // flyweights in the initialization stage of the application
+            var factory = new FlyweightFactory(
+                new Car { Company = "Chevrolet", Model = "Camaro2018", Color = "pink" },
+                new Car { Company = "Mercedes Benz", Model = "C300", Color = "black" },
+                new Car { Company = "Mercedes Benz", Model = "C500", Color = "red" },
+                new Car { Company = "BMW", Model = "M5", Color = "red" },
+                new Car { Company = "BMW", Model = "X6", Color = "white" }
+            );
+
+            Assert.Equal("Camaro2018_Chevrolet_pink\nblack_C300_Mercedes Benz\nC500_Mercedes Benz_red\nBMW_M5_red\nBMW_white_X6", factory.ListFlyweights());
+
+            Assert.Equal(
+                "Flyweight: Displaying shared {\"Owner\":null,\"Number\":null,\"Company\":\"BMW\",\"Model\":\"M5\",\"Color\":\"red\"} and unique {\"Owner\":\"James Doe\",\"Number\":\"CL234IR\",\"Company\":\"BMW\",\"Model\":\"M5\",\"Color\":\"red\"} state.", 
+                AddCarToPoliceDatabase(factory, new Car
+                {
+                    Number = "CL234IR",
+                    Owner = "James Doe",
+                    Company = "BMW",
+                    Model = "M5",
+                    Color = "red"
+                }));
+
+            Assert.Equal(
+                "Flyweight: Displaying shared {\"Owner\":null,\"Number\":null,\"Company\":\"BMW\",\"Model\":\"X1\",\"Color\":\"red\"} and unique {\"Owner\":\"James Doe\",\"Number\":\"CL234IR\",\"Company\":\"BMW\",\"Model\":\"X1\",\"Color\":\"red\"} state.",
+                AddCarToPoliceDatabase(factory, new Car
+                {
+                    Number = "CL234IR",
+                    Owner = "James Doe",
+                    Company = "BMW",
+                    Model = "X1",
+                    Color = "red"
+                }));
+
+            Assert.Equal("Camaro2018_Chevrolet_pink\nblack_C300_Mercedes Benz\nC500_Mercedes Benz_red\nBMW_M5_red\nBMW_white_X6\nBMW_red_X1", factory.ListFlyweights());
+        }
+
+        string AddCarToPoliceDatabase(FlyweightFactory factory, Car car)
+        {
+            var flyweight = factory.GetFlyweight(new Car
+            {
+                Color = car.Color,
+                Model = car.Model,
+                Company = car.Company
+            });
+
+            return flyweight.Operation(car);
+        }
+    }
+}
---
+++ DesignPatterns/Flyweight/Flyweight.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace DesignPatterns.Flyweight
+{
+    // source: https://refactoring.guru/design-patterns/flyweight/csharp/example
+
+    // The Flyweight stores a common portion of the state (also called intrinsic
+    // state) that belongs to multiple real business entities. The Flyweight
+    // accepts the rest of the state (extrinsic state, unique for each entity)
+    // via its method parameters.
+    public class Flyweight
+    {
+        Car SharedState { get; set; }
+
+        public Flyweight(Car car /* intrinsic state */)
+        {
+            SharedState = car;
+        }
+
+        public string Operation(Car uniqueState /* extrinsic state */)
+        {
+            string s = JsonSerializer.Serialize(SharedState);
+            string u = JsonSerializer.Serialize(uniqueState);
+            return $"Flyweight: Displaying shared {s} and unique {u} state.";
+        }
+    }
+
+    // The Flyweight Factory creates and manages the Flyweight objects. It
+    // ensures that flyweights are shared correctly. When the client requests a
+    // flyweight, the factory either returns an existing instance or creates a
+    // new one, if it doesn't exist yet.
+    public class FlyweightFactory
+    {
+        List<Tuple<Flyweight, string>> Flyweights { get; } = new List<Tuple<Flyweight, string>>();
+
+        public FlyweightFactory(params Car[] args)
+        {
+            args.ToList().ForEach(elem => Flyweights.Add(new Tuple<Flyweight, string>(new Flyweight(elem), GetKey(elem))));
+        }
+
+        string GetKey(Car car)
+        {
+            var elements = new List<string?> { car.Model, car.Color, car.Company, car.Owner, car.Number }
+                                .Where(k => k != null)
+                                .ToList();
+
+            elements.Sort();
+
+            return string.Join("_", elements);
+        }
+
+        public Flyweight GetFlyweight(Car sharedState)
+        {
+            string key = GetKey(sharedState);
+
+            if (!Flyweights.Where(t => t.Item2 == key).Any())
+                Flyweights.Add(new Tuple<Flyweight, string>(new Flyweight(sharedState), key));
+
+            return Flyweights.Where(t => t.Item2 == key).First().Item1;
+        }
+
+        public string ListFlyweights() => string.Join('\n', Flyweights.Select(f => f.Item2));
+    }
+
+    public class Car
+    {
+        public string? Owner { get; set; }
+        public string? Number { get; set; }
+        public string? Company { get; set; }
+        public string? Model { get; set; }
+        public string? Color { get; set; }
+    }
+}
