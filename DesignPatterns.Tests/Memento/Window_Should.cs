6cf1cb79ede6ff98ae453c543673f42683f4b5a4
Johnny Oshika <johnnyoshika@gmail.com>
2019-11-05 09:56:38 -0800

    Add Memento example

---
+++ DesignPatterns.Tests/Memento/Window_Should.cs
@@ -0,0 +1,53 @@
+﻿using DesignPatterns.Memento;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPatterns.Tests.Memento
+{
+    public class Window_Should
+    {
+        [Fact]
+        public void Store_First_State()
+        {
+            var window = new Window();
+
+            Assert.Equal(1, window.StateCount);
+            Assert.Empty(window.Print());
+        }
+
+        [Fact]
+        public void Store_Multiple_States_And_Undo()
+        {
+            var window = new Window();
+
+            window.Canvas.Strokes.Add("horizontal line");
+            window.StoreState();
+
+            window.Canvas.Strokes.Add("vertical line");
+            window.StoreState();
+
+            window.Canvas.Strokes.Add("circle");
+            window.StoreState();
+
+            Assert.Equal(4, window.StateCount);
+            Assert.Equal("horizontal line|vertical line|circle", window.Print());
+
+            window.Undo();
+
+            Assert.Equal(3, window.StateCount);
+            Assert.Equal("horizontal line|vertical line", window.Print());
+
+            window.Undo();
+
+            Assert.Equal(2, window.StateCount);
+            Assert.Equal("horizontal line", window.Print());
+
+            window.Undo();
+
+            Assert.Equal(1, window.StateCount);
+            Assert.Empty(window.Print());
+        }
+    }
+}
---
+++ DesignPatterns/Memento/Memento.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Memento
+{
+    // Most common use case: undo/redo stack
+    // 3 components:
+    // - The originator represents the object whose state is being tracked (Canvas in our example below)
+    // - The caretaker performs operations on the originator and needs to be able to undo the operations (Window in our example below)
+    // - The memento is a value object that holds the state of the originator (CanvasMemento in our example below)
+    public class Window
+    {
+        public Window() => StoreState();
+
+        Stack<IMemento> States { get; } = new Stack<IMemento>();
+        public Canvas Canvas { get; } = new Canvas();
+
+        public void StoreState() => States.Push(Canvas.CreateMemento());
+
+        public void Undo()
+        {
+            if (States.Count > 1)
+            {
+                // discard current stats
+                States.Pop();
+                Canvas.SetMemento(States.Peek());
+            }
+        }
+
+        public string Print() => string.Join('|', Canvas.Strokes);
+
+        public int StateCount => States.Count;
+    }
+
+    public interface IMemento
+    {
+        object State { get; }
+    }
+
+    public class Canvas
+    {
+        public List<string> Strokes { get; set; } = new List<string>();
+
+        public IMemento CreateMemento() => new CanvasMemento(Strokes.ToArray() /* creates a copy */);
+
+        public void SetMemento(IMemento memento) => Strokes = ((string[])memento.State).ToList();
+
+        public class CanvasMemento : IMemento
+        {
+            public CanvasMemento(object state) => State = state;
+            public object State { get; }
+        }
+    }
+}
