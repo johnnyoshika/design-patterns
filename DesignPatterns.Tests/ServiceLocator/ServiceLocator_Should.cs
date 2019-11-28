be7c240f792cb8ad770ce8043b2e7cf7451e2d03
Johnny Oshika <johnnyoshika@gmail.com>
2019-11-28 09:02:19 -0800

    Add ServiceLocator example

---
+++ DesignPatterns.Tests/ServiceLocator/ServiceLocator_Should.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+using SL = DesignPatterns.ServiceLocator.ServiceLocator;
+
+namespace DesignPatterns.Tests.ServiceLocator
+{
+    public class ServiceLocator_Should
+    {
+        [Fact]
+        public void Locate_Service()
+        {
+            var myTalker = new Mock<ITalker>();
+            myTalker.Setup(t => t.Speak()).Returns("Hi!");
+
+            SL.AddService("talker", myTalker.Object);
+
+            var talker = SL.GetService<ITalker>("talker");
+            Assert.Equal("Hi!", talker.Speak());
+        }
+    }
+
+    public interface ITalker
+    {
+        string Speak();
+    }
+}
---
+++ DesignPatterns/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.ServiceLocator
+{
+    public class ServiceLocator
+    {
+        static readonly Hashtable _services = new Hashtable();
+
+        public static void AddService<T>(string name, T t) => _services.Add(name, t);
+        public static T GetService<T>(string name) => (T)(_services[name] ?? throw new NotSupportedException());
+    }
+}
