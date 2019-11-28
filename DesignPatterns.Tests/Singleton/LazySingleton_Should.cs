2197969bb72a18eba609524d6d1a98432d63618d
Johnny Oshika <johnnyoshika@gmail.com>
2019-11-28 08:21:57 -0800

    Add Singleton example

---
+++ DesignPatterns.Tests/Singleton/LazySingleton_Should.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPatterns.Tests.Singleton
+{
+    public class LazySingleton_Should
+    {
+        [Fact]
+        public void Reference_Single_Instance()
+        {
+            var first = LazySingleton.Instance;
+            var second = LazySingleton.Instance;
+            Assert.Equal(first, second);
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/LazySingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public class LazySingleton
+    {
+        LazySingleton()
+        {
+        }
+
+        // This is thread safe.
+        public static LazySingleton Instance => Nested.instance;
+
+        public void DoSomething()
+        {
+        }
+
+        class Nested
+        {
+            // C# compiler will ensure that type initializer is instantiated lazily when there's a static constructor.
+            static Nested()
+            {
+            }
+
+            internal static readonly LazySingleton instance = new LazySingleton();
+        }
+    }
+}
