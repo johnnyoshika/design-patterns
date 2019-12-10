c6afb307d85f44d139f5b158ecf99794c77dfa5d
Johnny Oshika <johnnyoshika@gmail.com>
2019-12-10 08:38:24 -0800

    Add Template Method example

---
+++ DesignPatterns.Tests/TemplateMethod/OrderShipment_Should.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DesignPatterns.TemplateMethod;
+
+namespace DesignPatterns.Tests.TemplateMethod
+{
+    public class OrderShipment_Should
+    {
+        [Fact]
+        public void Ship_Ups()
+        {
+            var logger = new Mock<ILogger>();
+            var ups = new UpsOrderShipment();
+            ups.Address = "123 Broadway";
+            ups.Ship(logger.Object);
+
+            logger.Verify(l => l.Log("UPS:[123 Broadway]"), Times.Once());
+        }
+
+        [Fact]
+        public void Ship_FedEx()
+        {
+            var logger = new Mock<ILogger>();
+            var ups = new FedExOrderShipment();
+            ups.Address = "123 Broadway";
+            ups.Ship(logger.Object);
+
+            logger.Verify(l => l.Log("FedEx:[123 Broadway]"), Times.Once());
+        }
+    }
+}
---
+++ DesignPatterns/TemplateMethod/OrderShipment.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DesignPatterns.TemplateMethod
+{
+    // Templete Method uses inheritance to define an algorithm in a superclass while delegating responsibility for detailed implementations to child classes.
+    // It provides greater reuse but it's less flexible than the Strategy pattern.
+
+    // Template Method embodies the Hollywood Principle: Don't call us, we'll call you
+
+    public abstract class OrderShipment
+    {
+        public string? Address { get; set; }
+        public string? Label { get; set; }
+
+        // This is the template method.
+        public void Ship(ILogger logger)
+        {
+            VerifyShippingData();
+            GetShippingLabelFromCarrier();
+            PrintLabel(logger);
+        }
+
+        public virtual void VerifyShippingData()
+        {
+            if (string.IsNullOrWhiteSpace(Address))
+                throw new ApplicationException("Invalid address.");
+        }
+
+        public abstract void GetShippingLabelFromCarrier();
+
+        public virtual void PrintLabel(ILogger logger) =>
+            logger.Log(Label);
+    }
+
+    public class UpsOrderShipment : OrderShipment
+    {
+        public override void GetShippingLabelFromCarrier() =>
+            // Call UPS web service
+            Label = $"UPS:[{Address}]";
+    }
+
+    public class FedExOrderShipment : OrderShipment
+    {
+        public override void GetShippingLabelFromCarrier() =>
+            // Call FedEx web service
+            Label = $"FedEx:[{Address}]";
+    }
+}
